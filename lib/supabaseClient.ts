08b266d1a199848743e74e81311ef63f3ea1ab2d
Christian Rogers <christianrodges@bellsouth.net>
2026-03-21 09:25:24 -0400

    Fix Supabase anon key

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://nqzoizfekbrqxsvkpkgr.supabase.co",
-  "sb_publishable_RLUk0zYKIX9PJFSRaS-vVA_H0-8vJHj",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xem9pemZla2JycXhzdmtwa2dyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMDYzMDgsImV4cCI6MjA4Nzg4MjMwOH0.xGi57urnqcqc5syLgzpWpgC5MglWIhrCttWF8CBTY-Y",
   {
     auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
   }
